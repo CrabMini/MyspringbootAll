94c684560bbf2a9c0b880bc5b453db64f3a5e621
CrabMini <1143897046@qq.com>
2020-05-29 15:20:35 +0800

    xxx

---
+++ 08Dubbo-zk/src/main/java/com/crab/dubbozk/controller/DepartmentController.java
@@ -0,0 +1,34 @@
+package com.crab.dubbozk.controller;
+
+import com.crab.dubbozk.mapper.DepartmentMapper;
+import com.crab.dubbozk.pojo.Department;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class DepartmentController {
+
+    @Autowired
+    DepartmentMapper departmentMapper;
+
+    // 查询全部部门
+    @GetMapping("/getDepartments")
+    public List<Department> getDepartments(){
+        return departmentMapper.getDepartments();
+    }
+
+    // 查询全部部门
+    /*
+    例如id=1
+    url=  /getDepartment/1
+     */
+    @GetMapping("/getDepartment/{id}")
+    public Department getDepartment(@PathVariable("id") Integer id){
+        return departmentMapper.getDepartment(id);
+    }
+
+}
---
+++ 08Dubbo-zk/src/main/java/com/crab/dubbozk/pojo/Department.java
@@ -0,0 +1,16 @@
+package com.crab.dubbozk.pojo;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Department {
+
+    private Integer id;
+    private String departmentName;
+
+}
