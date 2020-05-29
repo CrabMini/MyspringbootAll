b81b4228acec3cc180e69814fc24a65c42e3790c
CrabMini <1143897046@qq.com>
2020-05-29 14:25:24 +0800

    bases-editor

---
+++ 08Editor-Md/src/main/java/com/crab/editormd/EditorMdApplication.java
@@ -0,0 +1,13 @@
+package com.crab.editormd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EditorMdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EditorMdApplication.class, args);
+    }
+
+}
