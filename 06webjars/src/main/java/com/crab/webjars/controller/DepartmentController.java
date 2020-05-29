ae8fe0b695bbbe8a472c3725cb2a76e11eb6e6e5
CrabMini <1143897046@qq.com>
2020-05-29 10:48:03 +0800

    Thymeleaf

---
+++ 06webjars/src/main/java/com/crab/webjars/controller/DepartmentController.java
@@ -0,0 +1,34 @@
+package com.crab.webjars.controller;
+
+import com.crab.webjars.mapper.DepartmentMapper;
+import com.crab.webjars.pojo.Department;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class DepartmentController {
+
+    @Autowired
+    DepartmentMapper departmentMapper;
+
+    // 查询全部部门
+    @GetMapping("/getDepartments")
+    public List<Department> getDepartments(){
+        return departmentMapper.getDepartments();
+    }
+
+    // 查询全部部门
+    /*
+    例如id=1
+    url=  /getDepartment/1
+     */
+    @GetMapping("/getDepartment/{id}")
+    public Department getDepartment(@PathVariable("id") Integer id){
+        return departmentMapper.getDepartment(id);
+    }
+
+}
