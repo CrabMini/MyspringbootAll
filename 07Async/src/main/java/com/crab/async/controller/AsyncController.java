3c5e9162922a5d439a855606a4c040f0ff81935b
CrabMini <1143897046@qq.com>
2020-05-29 11:47:51 +0800

    异步请求&定时任务

---
+++ 07Async/src/main/java/com/crab/async/controller/AsyncController.java
@@ -0,0 +1,27 @@
+package com.crab.async.controller;
+
+import com.crab.async.service.AsyncService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AsyncController {
+
+    @Autowired
+    AsyncService asyncService;
+
+    @GetMapping("/hello")
+    //告诉Spring这是一个异步方法
+    @Async
+    public void hello(){
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("业务进行中....");
+    }
+
+}
